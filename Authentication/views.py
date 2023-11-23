3f1e8ff791efc9aa4cbac0e14ddd767fcae2c483
Mahdi Peydai <mahdi@Mahdis-MacBook-Pro.local>
2023-11-24 01:46:57 +0330

    login view is created, urls is set for login view

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from .forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth import authenticate
+from django.contrib import messages
+from .authentication import admin_authentication
 
 
 class RegistrationView(APIView):
@@ -19,4 +22,30 @@ class RegistrationView(APIView):
             return render(request, 'authentication/authentication.html',
                           {'reg_form': reg_form, 'login_form': login_form, 'default': 'registration'})
 
+
+class LoginView(APIView):
+    def get(self, request):
+        login_form = UserLoginForm()
+        reg_form = UserRegisterForm
+        return render(request, 'authentication/authentication.html',
+                      {'reg_form': reg_form, 'login_form': login_form, 'default': 'login'})
+
+    def post(self, request):
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                if admin_authentication(user):
+                    return
+                else:
+                    messages.error(request, 'شما به پنل دسترسی ندارید')
+                    return redirect('login')
+            else:
+                messages.error(request, 'ایمیل یا رمز عبور صحیح نمی‌باشد')
+                return redirect('login')
+        else:
+            messages.error(request, 'ایمیل یا رمز عبور صحیح نمی‌باشد')
+            return redirect('login')
 
