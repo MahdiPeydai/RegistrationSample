5289ddb81dc62ddc2e1694c47f5fac54ce60bde2
Mahdi Peydai <mahdi@Mahdis-MacBook-Pro.local>
2023-11-24 10:37:07 +0330

    restful APIs is created for User model, panel view is created

--- panel/views.py
+++ panel/views.py
@@ -1,3 +1,24 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from Authentication.models import User
+from .serializers import UserSerializer
+from .permissions import IsAdmin
+
+
+class PanelView(View):
+    def get(self, request):
+        return render(request, 'panel/admin_panel.html')
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdmin]
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdmin]
