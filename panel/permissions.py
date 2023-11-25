85edb25eecacce3c2d23c84b69c6ce920d9badbe
Mahdi Peydai <mahdi@Mahdis-MacBook-Pro.local>
2023-11-25 10:14:05 +0330

    panel view is completed, UserList is deleted(no usage), user update view is created, admin permission is added to all views

--- panel/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import permissions
-from Authentication.models import User
-
-
-class IsAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_superuser
--- panel/views.py
+++ panel/views.py
@@ -1,24 +1,30 @@
 from django.views import View
 from django.shortcuts import render
-
 from rest_framework import generics
+
 from Authentication.models import User
+from .forms import UserUpdateForm
 from .serializers import UserSerializer
-from .permissions import IsAdmin
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import permission_required
 
 
+@method_decorator(permission_required('is_superuser'), name='dispatch')
 class PanelView(View):
     def get(self, request):
-        return render(request, 'panel/admin_panel.html')
+        users = User.objects.all()
+        return render(request, 'panel/admin_panel.html', {'users': users})
 
 
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAdmin]
+@method_decorator(permission_required('is_superuser'), name='dispatch')
+class UserUpdateView(View):
+    def get(self, request, pk):
+        user = User.objects.get(id=pk)
+        form = UserUpdateForm(instance=user)
+        return render(request, 'panel/edit_page.html', {'form': form, 'pk': pk})
 
 
+@method_decorator(permission_required('is_superuser'), name='dispatch')
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdmin]
