3945d18c26b13a4c9c4a1de21407befae3b7bd4f
Mahdi Peydai <mahdi@Mahdis-MacBook-Pro.local>
2023-11-25 10:22:05 +0330

    panel urls is added to project, pre-name of authentication urls is modified

--- RegisterationSample/urls.py
+++ RegisterationSample/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('Authentication.urls')),
-    path('', include('panel.urls'))
+    path('accounts/', include('Authentication.urls')),
+    path('panel/', include('panel.urls'))
 ]
