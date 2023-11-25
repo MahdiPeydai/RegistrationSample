e9b82c0e66d7e0c53fdbc9946103f0967eadc123
Mahdi Peydai <mahdi@Mahdis-MacBook-Pro.local>
2023-11-25 10:25:45 +0330

    id and date_joined is added to serilizer fields, email id removed from read-only fields

--- panel/serializers.py
+++ panel/serializers.py
@@ -5,4 +5,5 @@ from Authentication.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'phone']
+        fields = ['id', 'email', 'first_name', 'last_name', 'phone', 'date_joined']
+        read_only_fields = ['id', 'date_joined']
